82a1824cb6451ee5ada956674fc4de49319b290d
Nick Ribal <elektronik2k5@gmail.com>
2019-05-22 23:32:36 +0200

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { getStore } from './stores/rootStore'
 
 it('renders without crashing', () => {
+  const rootStore = getStore()
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App {...rootStore} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
