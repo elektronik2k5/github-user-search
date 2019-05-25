5d78b91236d4979c2ca42c8f1a97713be14f00e3
Nick Ribal <elektronik2k5@gmail.com>
2019-05-25 02:08:18 +0200

    Updates

---
+++ precache-manifest.2c72104f05ee4a7e6889c710b5f529a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f14fc1a6827db682e89e4cee4ddfd7",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "fb1ce4db49ede40c619e",
+    "url": "/github-user-search/static/js/2.7a5f60e3.chunk.js"
+  },
+  {
+    "revision": "83b79df3e665772134fb",
+    "url": "/github-user-search/static/js/main.5796d521.chunk.js"
+  },
+  {
+    "revision": "1929e8f15580a5d437fd",
+    "url": "/github-user-search/static/js/runtime~main.e5dc5a1d.js"
+  }
+]);
