344de1c16b956db7b9e2fc4249f96914e1890b7c
Nick Ribal <elektronik2k5@gmail.com>
2021-11-03 12:34:57 -0600

    Add servicebell

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
 import { getStore } from './stores/rootStore'
+import ServiceBell from '@servicebell/widget'
 
 function renderApp(rootElement: HTMLElement | null) {
   if (!rootElement) {
@@ -14,6 +15,7 @@ function renderApp(rootElement: HTMLElement | null) {
     </StrictMode>,
     document.getElementById('root'),
   )
+  ServiceBell.init('4431fbab7414459c94726cad5962facf')
 }
 
 renderApp(document.getElementById('root'))
